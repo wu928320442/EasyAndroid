9b2dc816ea88c66822617f3db42bf8403ca6b644
wujiajun <wujiajun@qbao.com>
2017-04-14 17:35:58 +0800

    代码重构

---
+++ app/src/main/java/com/wjj/easy/easyandroidHelper/module/dialog/BaseDialog.java
@@ -0,0 +1,83 @@
+package com.wjj.easy.easyandroidHelper.module.dialog;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Gravity;
+import android.view.Window;
+import android.view.WindowManager.LayoutParams;
+
+import com.wjj.easy.easyandroidHelper.R;
+
+import butterknife.ButterKnife;
+
+
+/**
+ * Created by zhaotun
+ */
+public abstract class BaseDialog extends Dialog {
+
+    protected float mDimAmount = 0.6f;
+
+    protected float mWidthScale = 0.9f;
+
+    protected int gravity = Gravity.CENTER_VERTICAL;
+
+    protected int animId = -1;
+
+    protected int x = 0;
+
+    protected int y = 0;
+
+    protected int h = LayoutParams.WRAP_CONTENT;
+
+    protected boolean mShouldSetWindow = true;
+
+
+    public BaseDialog(Context context) {
+        super(context, R.style.dialog_custom_roll_up_down);
+    }
+
+    public BaseDialog(Context context, int theme) {
+        super(context, theme);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getContentView());
+        ButterKnife.bind(this);
+        init();
+
+        if (mShouldSetWindow) {
+            setWindow(x, y);
+        }
+    }
+
+    protected void setWindow(int x, int y) {
+        Window dialogWindow = getWindow();
+        LayoutParams lp = dialogWindow.getAttributes();
+        DisplayMetrics d = getContext().getResources().getDisplayMetrics(); // 获取屏幕宽、高用
+        lp.width = (int) (d.widthPixels * mWidthScale);
+        lp.height = h;
+        lp.x = x;
+        lp.y = y;
+        lp.gravity = gravity;
+        lp.dimAmount = mDimAmount;
+        dialogWindow.addFlags(LayoutParams.FLAG_DIM_BEHIND);
+
+        if (animId != -1) {
+            dialogWindow.setWindowAnimations(animId);
+        }
+    }
+
+    public void shouldSetWindow(boolean shouldSetWindow) {
+        this.mShouldSetWindow = shouldSetWindow;
+    }
+
+    protected abstract int getContentView();
+
+    protected abstract void init();
+
+}
---
+++ app/src/main/java/com/wjj/easy/easyandroidHelper/module/dialog/DialogLoading.java
@@ -0,0 +1,48 @@
+package com.wjj.easy.easyandroidHelper.module.dialog;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.TextView;
+
+import com.wjj.easy.easyandroidHelper.R;
+
+import butterknife.BindView;
+
+
+/**
+ * Created by zhaotun on 2017/2/21.
+ */
+
+public class DialogLoading extends BaseDialog {
+
+    @BindView(R.id.tv_loading_text)
+    TextView tvText;
+
+    public DialogLoading(Context context) {
+        this(context, false);
+    }
+
+    public DialogLoading(Context context, boolean shouldSetWindow) {
+        super(context, R.style.customDialog_loading);
+        shouldSetWindow(shouldSetWindow);
+
+        setCanceledOnTouchOutside(false);
+        setCancelable(true);
+    }
+
+    @Override
+    protected int getContentView() {
+        return R.layout.dialog_loading;
+    }
+
+    @Override
+    protected void init() {
+
+    }
+
+    public void setText(String text) {
+        if (tvText != null) {
+            tvText.setText(text);
+        }
+    }
+}
